e57369f3c10218199ccc5aa1824f553ba65e32c6
wanderson <wanderson.medeiros2@gmail.com>
2015-06-10 20:58:54 -0300

    Novas activitys

--- app/src/main/java/br/ufrn/jardim/appjardim/CadastrarAlertaActivity.java
+++ app/src/main/java/br/ufrn/jardim/appjardim/CadastrarAlertaActivity.java
@@ -6,19 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class ConfiguracoesActivity extends ActionBarActivity {
+public class CadastrarAlertaActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_configuracoes);
+        setContentView(R.layout.activity_cadastrar_alerta);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_configuracoes, menu);
+        getMenuInflater().inflate(R.menu.menu_cadastrar_alerta, menu);
         return true;
     }
 
